77cd417090f52cbf997cff3dc756772b9fe650b3
David Nguyen <david.nguyen16@mavs.uta.edu>
2021-11-04 17:43:56 -0500

    initial setup

---
+++ ChatNow/LoginViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LoginViewController.swift
+//  ChatNow
+//
+//  Created by David  on 11/4/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  
+
+    @IBAction func onSignIn(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
