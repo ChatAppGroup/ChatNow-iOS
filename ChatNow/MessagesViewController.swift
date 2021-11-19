c6a155cf55b18516d5067e6132b7f5e0f36915b0
David Nguyen <kiwiqv23@gmail.com>
2021-11-18 20:18:13 -0600

    Display cells in Message View Controller

--- ChatNow/MessagesViewController.swift
+++ ChatNow/MessagesViewController.swift
@@ -7,10 +7,16 @@
 
 import UIKit
 
-class MessagesViewController: UIViewController {
-
+class MessagesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MessageCell")
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +27,25 @@ class MessagesViewController: UIViewController {
         UserDefaults.standard.setValue(false, forKey: "loggedin")
         self.dismiss(animated: true, completion: nil )
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+        cell.textLabel?.text = "David"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false )
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
